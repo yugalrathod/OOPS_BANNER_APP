f59bed559d4cfc5853f2c9d5e4df3019d18515d0
yugalrathod <yugalrathod3388@gmail.com>
2026-02-16 09:00:02 +0530

    hey

---
+++ oopsbanner.java
@@ -0,0 +1,14 @@
+public class OOPS_BANNER_APP {
+
+    public static void main(String[] args) {
+        System.out.println( "  *****  " + "  *****  " + "  ****** " + "  ****** " );
+        System.out.println( " *     * " + " *     * " + " *     * " + " *      ");
+        System.out.println( " *     * " + " *     * " + " *     * " + " *      ");
+        System.out.println( " *     * " + " *     * " + " ******  " + "  *****  " );
+        System.out.println( " *     * " + " *     * " + " *       " + "       * ");
+        System.out.println( " *     * " + " *     * " + " *       " + "       * " );
+        System.out.println( "  *****  " + "  *****  " + " *       " + "  *****  ");
+        System.out.println("hello");
+
+    }
+}
