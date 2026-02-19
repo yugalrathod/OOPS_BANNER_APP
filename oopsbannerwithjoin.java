072acc7f78ee845d7ce53df01515bbf5d8758c66
yugalrathod <yugalrathod3388@gmail.com>
2026-02-19 23:54:14 +0530

    hey

---
+++ oopsbannerwithjoin.java
@@ -0,0 +1,22 @@
+public class oopsbannerwithjoin {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                "  *****  ", "  *****  ", "  ****** ", "  ****** "));
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *     * ", " *      "));
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *     * ", " *      "));
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " ******  ", "  *****  "));
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *       ", "       * "));
+        System.out.println(String.join("",
+                " *     * ", " *     * ", " *       ", "       * "));
+        System.out.println(String.join("",
+                "  *****  ", "  *****  ", " *       ", "  *****  "));
+
+        System.out.println("hello");
+    }
+}
