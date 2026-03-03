22bacc3e4ccb593452e06874037ded65a2d09a9e
yugalrathod <yugalrathod3388@gmail.com>
2026-03-03 08:39:08 +0530

    UC6-DONE

---
+++ UC_06.java
@@ -0,0 +1,49 @@
+public class UC_06 {
+
+    public static void main(String[] args) {
+
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
+
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
+        }
+    }
+
+    static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    static String[] buildP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    static String[] buildS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+}
